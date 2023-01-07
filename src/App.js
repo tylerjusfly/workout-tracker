95237791fdb7c3bfdc4fa1f6d7a0d6df05809667
tylerjusfly <tylerjusfly1@gmail.com>
2023-01-07 02:14:57 +0100

    first commit bumping up pushups by 10

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, createRef } from "react";
+import OnlyGod from "./Only-God-Can-Judge-Me.mp3";
 
 function App() {
+  const [pushupCount, setPushupCount] = useState(JSON.parse(localStorage.getItem("pushup")) || 0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const audioRef = createRef();
+
+  useEffect(() => {
+    console.log("calledon relaod");
+    localStorage.setItem("pushup", JSON.stringify(pushupCount));
+  }, [pushupCount]);
+
+  const bumpupCount = () => {
+    setPushupCount((prevVal) => prevVal + 10);
+    audioRef.current.play();
+    setIsPlaying(true);
+  };
+
+  setTimeout(() => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    }
+  }, 3000);
+
+  const resetPushupCount = () => {
+    setPushupCount(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h3>{pushupCount} Push Up Done</h3>
+      <div className="pushup-btns">
+        <button className="button accept-btn" onClick={bumpupCount}>
+          Bump Up
+        </button>
+        <button className="button cancel-btn pad" onClick={resetPushupCount}>
+          Reset For The Day
+        </button>
+      </div>
+      <audio ref={audioRef} src={OnlyGod} />
     </div>
   );
 }
